a7fdd0e72c1bdeb1a1d1aa0a6037cf196c34c6a6
ABChe <abche777@163.com>
2017-03-20 14:43:17 +0800

    Initial Commit

---
+++ MoreOperationToolBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoreOperationToolBar
+//
+//  Created by 车 on 17/3/20.
+//  Copyright © 2017年 abche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoreOperationToolBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoreOperationToolBar
+//
+//  Created by 车 on 17/3/20.
+//  Copyright © 2017年 abche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
